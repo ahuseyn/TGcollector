eb4c77a5e2ae618d3237f46f9a7f51d6e5ab75db
ahuseyn <husen.agaev@gmail.com>
2024-04-03 22:51:13 +0200

    downloadFile fixes

--- src/helpers/downloadFile.js
+++ src/helpers/downloadFile.js
@@ -6,33 +6,41 @@ import serveFile from "helpers/serveFile";
 
 export async function downloadFile(job, name, format) {
   if (!Boolean(format)) {
-    return toast.error("Please choose a format to save");
+    return toast.error("Please choose a format to save", {
+      id: "download-file",
+    });
   }
 
-  const db = await openDB(job);
-  const data = await db.getAll("messages");
+  try {
+    const db = await openDB(job);
+    const data = await db.getAll("messages");
 
-  const groupedData = data.flat();
+    const groupedData = data.flat();
 
-  const fileName = name.replace(/[^a-z0-9]/gi, "_").toLowerCase();
+    const fileName = name.replace(/[^a-z0-9]/gi, "_").toLowerCase();
 
-  switch (format) {
-    case "json":
-      {
-        const jsonData = JSON.stringify(groupedData, replacer, 2);
-        const blob = new Blob([jsonData], { type: "application/json" });
-        serveFile(blob, `${fileName}.json`);
-      }
-      break;
-    case "csv":
-      {
-        const cleanedData = bigintConvert(groupedData);
-        const csvData = await json2csv(cleanedData, {});
-        const blob = new Blob([csvData], { type: "text/csv" });
-        serveFile(blob, `${fileName}.csv`);
-      }
-      break;
-    default:
-      toast.error("Please provide a valid file format");
+    switch (format) {
+      case "json":
+        {
+          const jsonData = JSON.stringify(groupedData, replacer, 2);
+          const blob = new Blob([jsonData], { type: "application/json" });
+          serveFile(blob, `${fileName}.json`);
+        }
+        break;
+      case "csv":
+        {
+          const cleanedData = bigintConvert(groupedData);
+          const csvData = await json2csv(cleanedData, {});
+          const blob = new Blob([csvData], { type: "text/csv" });
+          serveFile(blob, `${fileName}.csv`);
+        }
+        break;
+      default:
+        toast.error("Please provide a valid file format");
+    }
+  } catch (err) {
+    // TODO send crash report
+    console.error("Error on message collection: ", err);
+    toast.error("File could not be prepared. " + err, { id: "download-file" });
   }
 }
